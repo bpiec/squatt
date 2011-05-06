8e0240225e71d929e362b828bb7ec6d62b3e55c5
bartosz.piec <bartosz.piec@f16d0824-ca25-11dd-86d7-074df07e0730>
2011-05-06 08:31:25 +0000

    MySQL Provider

---
+++ Dabarto.Data.Squatt.Providers.MySQL/MySQLSquattProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Dabarto.Data.Squatt.Data.Providers
+{
+	public class SQLiteSquattProvider : SquattProvider
+	{
+		public override string IdentifierEnclosingStartChar
+		{
+			get
+			{
+				return "`";
+			}
+		}
+
+		public override string IdentifierEnclosingEndChar
+		{
+			get
+			{
+				return "`";
+			}
+		}
+
+		public SQLiteSquattProvider()
+		{
+		}
+
+		public override int ExecuteNonQuery(string query)
+		{
+			using (var connection = new MySqlConnection(Configuration.ConnectionString))
+			{
+				var command = connection.CreateCommand();
+				command.CommandType = CommandType.Text;
+				command.CommandText = query;
+				return command.ExecuteNonQuery();
+			}
+		}
+
+		public override DataTable ExecuteQuery(string query)
+		{
+			var dataTable = new DataTable();
+
+			using (var connection = new MySqlConnection(Configuration.ConnectionString))
+			{
+				var adapter = new MySqlDataAdapter(query, connection);
+				adapter.Fill(dataTable);
+			}
+
+			return dataTable;
+		}
+	}
+}
